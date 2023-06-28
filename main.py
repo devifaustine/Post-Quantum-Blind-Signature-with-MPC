cbcf84ede09c5c021a75a0b90e424566cf4cedfa
devifaustine <devifaustine.df@gmail.com>
2023-06-28 17:21:37 +0200

    Fix parameters and import error

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # This is the main file, where the post quantum blind signature scheme is implemented and benchmarked.
 
-import pyspx.shake256_128f
+import pyspx.shake_128f
+import random
+import string
 
-seed = 99
-public_key, secret_key = pyspx.shake256_128f.generate_keypair(seed)
-signature = pyspx.shake256_128f.sign(message, secret_key)
-ver = pyspx.shake256_128f.verify(message, signature, public_key)
+x = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(48))
+
+print("seed = %s" %x)
+seed = bytes(x, 'ascii')
+
+print(seed)
+
+message = b"Hello World!"
+
+public_key, secret_key = pyspx.shake_128f.generate_keypair(seed)
+signature = pyspx.shake_128f.sign(message, secret_key)
+ver = pyspx.shake_128f.verify(message, signature, public_key)
 print(ver)
