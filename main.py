17f34981d38f52fa6b18a0d20ff1c1b236916789
devifaustine <devifaustine.df@gmail.com>
2023-06-28 20:59:56 +0200

    Add timer and trying shake_256f

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 # This is the main file, where the post quantum blind signature scheme is implemented and benchmarked.
 
+import pyspx.shake_256f
 import pyspx.shake_128f
 import random
 import string
+import time
 
 x = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(48))
+y = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(96))
 
-print("seed = %s" %x)
-seed = bytes(x, 'ascii')
+list_seeds = [x, y]
+seeds = []
 
-print(seed)
+for i in range(len(list_seeds)):
+    print(i)
+    print("seed (string)= %s" %list_seeds[i])
+    seeds.append(bytes(list_seeds[i], 'ascii'))
+    print(seeds[i])
 
 message = b"Hello World!"
 
-public_key, secret_key = pyspx.shake_128f.generate_keypair(seed)
+start = time.time()
+public_key, secret_key = pyspx.shake_128f.generate_keypair(seeds[0])
 signature = pyspx.shake_128f.sign(message, secret_key)
 ver = pyspx.shake_128f.verify(message, signature, public_key)
+end = time.time()
+elapsed = end - start
 print(ver)
+print("time =", elapsed)
+
+start = time.time()
+public_key, secret_key = pyspx.shake_256f.generate_keypair(seeds[1])
+signature = pyspx.shake_256f.sign(message, secret_key)
+ver = pyspx.shake_256f.verify(message, signature, public_key)
+end = time.time()
+elapsed = end - start
+print(ver)
+print("time =", elapsed)
