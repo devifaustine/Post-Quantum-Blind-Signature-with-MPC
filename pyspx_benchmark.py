544f8c8ea52857698a07b21fc7a3e6dbf0219f9c
devifaustine <devifaustine.df@gmail.com>
2023-11-01 12:04:01 +0100

    Rename main.py

--- pyspx_benchmark.py
+++ pyspx_benchmark.py
@@ -1,13 +1,11 @@
-# This is the main file, where the post quantum blind signature scheme is implemented and benchmarked.
+# This executes a benchmark for the signing function of the official SPHINCS Library/Implementation,
+# the PySPX library over different inputs
 
 import pyspx.shake_256f
 import pyspx.shake_128f
 import random
 import string
 import time
-from statistics import mean
-import inspect
-import importlib
 
 # Create 2 seeds randomly, one for SHA128 the other for SHA256
 x = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(48))
@@ -34,7 +32,9 @@ for i in range(100):
     messages.append(bytes(message, 'ascii'))
     print("\t %s" %message)
 
+# list of elapsed time for SPHINCS using SHA128
 time_128 = []
+# list of elapsed time for SPHINCS using SHA256
 time_256 = []
 
 # Signing using SHA-128
@@ -72,12 +72,15 @@ print()
 
 elapsed_128 = 0
 elapsed_256 = 0
+
 for i in range(len(time_128)):
     elapsed_128 += time_128[i]
 for i in range(len(time_256)):
     elapsed_256 += time_256[i]
-print("Time required for SHA128 is %d seconds." %elapsed_128)
-print("Time required for SHA256 is %d seconds." %elapsed_256)
+
+# prints out the average time results
+print("Time required for SHA128 is %d seconds." %(elapsed_128 / len(time_128)))
+print("Time required for SHA256 is %d seconds." %(elapsed_256 / len(time_256)))
 print("SHA128 is %d times faster than SHA256" %(elapsed_128/elapsed_256))
 
 
