1be22b8dca3ab5e257cd7ca4182157406146d33d
devifaustine <devifaustine.df@gmail.com>
2023-11-27 16:44:14 +0100

    Fix check_type() and add split function

--- sphincs/mpyc_sphincs_benchmark.py
+++ sphincs/mpyc_sphincs_benchmark.py
@@ -14,11 +14,44 @@ end = time.time()
 elapsed = end - start
 print("time taken to generate the key: %d seconds" % elapsed)
 
-for i in range(len(key)):
-    print(type(key[i]))
+print("sk =", key[1])
 
 # runs the sign() function using MPC
 # maybe comment the time.time() and unnecessary code when using benchmarking tools from python later
+
+def q_split(q):
+    """
+    function to parse q in form of string
+    :param q: Q in SK
+    :return: Q in its original form a list of bytestring
+    """
+    res = []
+    for i in range(len(q)):
+        if i == 0:  # [ present at the first char
+            res.append(eval(q[i][1:]))
+            print(res[i])
+        elif i == len(q) - 1:
+            res.append(eval(q[i][:-1]))
+        else:
+            res.append(eval(q[i]))
+    return res
+
+
+def split_sk(sk):
+    """
+    split secret key sk (type = bytestring) into its components of type tuple
+    :param sk: secret key
+    :return: (sk1, sk2, q)
+    """
+    sk = sk.strip("()")  # Remove parentheses
+    sk_split_str = sk.split(", ")
+
+    sk1 = eval(sk_split_str[0])  # Using eval to convert the string back to bytes
+    sk2 = eval(sk_split_str[1])
+    q_str = sk_split_str[2:]
+    q = q_split(q_str)
+
+    return sk1, sk2, q
 
 def check_type(x):
     """
@@ -27,14 +60,15 @@ def check_type(x):
     :return: True/False
     """
     # case x is secret key
-    if x[0] == '(' and x[1] == '(':
+    if x[0] == '(' and x[-1] == ')':
         return True
-    # case x is a message
-    elif x[0] == 'b' and ord(x[1]) == 92:
+    # case x is a message, messages always starts with 'b/'
+    # elif x[0] == 'b' and ord(x[1]) == 92:
+    else:
         return False
-    raise ValueError("This is neither a message, nor a secret key!")
+    # raise ValueError("This is neither a message, nor a secret key!")
 
-async def bench(self):
+async def main():
     # run the sign() function build from mpyc and time it
 
     secfld = mpc.SecFld()
@@ -45,27 +79,28 @@ async def bench(self):
     # accept input from both user and signer
     payload = input('Give your input here: ')
 
-    # TODO: check the input type (message or sk) and use check_type() to determine the secure object
+    print("type of payload is: ", type(payload))
+    print(payload)
+
     try:
         if check_type(payload):
             # payload is a secret key
-            x = np.array([(b >> i) & 1 for b in X for i in range(8)])  # bytes to bits
-            x = secfld.array(x)  # secret-shared input bits
+            sk1, sk2, q = split_sk(payload)
+            # TODO:  convert each element of sk into secure obj (SecFld and array)
+            #x = np.array([(b >> i) & 1 for b in X for i in range(8)])  # bytes to bits
+            #x = secfld.array(x)  # secret-shared input bits
         else:
             # payload is a message
-            x = 
+            m = payload # TODO: but as secure object of type SecFld array of bytes
     except ValueError:
-        print("Payload invalid. Try Again!")
+        print("Payload invalid. check_type failed to recognize the pattern. Try Again!")
         await mpc.shutdown()
 
+    sig = sphincs.sign(m, sk)
 
-    # TODO: process both inputs from parties and sign the message with the sk
-    for i in range(len(payloads)):
-        print(payloads[i])
-    print("There's the payload")
+    print("Here is the signature desired: ", sig)
 
-    # TODO: outputs the blind signature before shutting down
     await mpc.shutdown()
 
-
-# TODO: verifies if the signature is correct and legit
+if __name__ == "__main__":
+    mpc.run(main())
