6b498a3fa5ebcdf6e9ea246614b0399ddd04cc00
devifaustine <devifaustine.df@gmail.com>
2024-02-14 16:07:09 +0100

    Add test for utils

--- sphincs/test_utils.py
+++ sphincs/test_utils.py
@@ -2,17 +2,58 @@
 
 from mpyc.runtime import mpc
 import numpy as np
-from sphincs import utils
+from utils import UTILS
 
+# initialize the utilities class and secure field s
+util = UTILS()
 secfld = mpc.SecFld(2)
 
+
+async def bits_to_bytestring(y):
+    """
+    converts x from array of bits of secure object to normal bytes
+    :param x: input of type secfld.array (secure array)
+    :return: bytes(x)
+    """
+    x = await mpc.output(y)
+    x_bitstring = ''.join(str(bit) for bit in x)
+
+    # Convert the binary string back to bytes
+    x_bytes = bytes(int(x_bitstring[i:i + 8], 2) for i in range(0, len(x_bitstring), 8))
+
+    return eval(x_bytes)
+
 # test bits_to_bytestring
-def test_bits_to_bytestring():
-    byte = b"hello"
+async def test_bits_to_bytestring(byte = b"hello"):
+    await mpc.start()
     string_bit = ''.join(format(ord(i), '08b') for i in str(byte))
 
     # payload is a list of secure objects containing the elements of
-    x = secfld.array(np.array([1, 0, 0, 0, 0, 0, 0, 0]))
-    assert utils.bits_to_bytestring(x) == byte
+    x = secfld.array(np.array([int(i) for i in string_bit]))
+    print("original byte: ", byte)
 
-test_bits_to_bytestring()
+    converted_byte = mpc.convert()
+    print(converted_byte)
+
+    #convert the bits to bytes
+    x = await mpc.output(x)
+    x_bitstring = ''.join(str(bit) for bit in x)
+
+    # Convert the binary string back to bytes
+    x_bytes = bytes(int(x_bitstring[i:i + 8], 2) for i in range(0, len(x_bitstring), 8))
+
+
+    out = await mpc.output(x_bytes)
+
+    print("converted byte: ", out)
+    assert out == byte
+    await mpc.shutdown()
+
+async def main():
+    await mpc.start()
+
+    await test_bits_to_bytestring(b'hello')
+
+    await mpc.shutdown()
+
+mpc.run(main())
