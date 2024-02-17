1c00aa40f6b73dee0ad4d2e86c1b65e20a51f936
devifaustine <devifaustine.df@gmail.com>
2024-02-17 15:05:29 +0100

    Add WOTS in XMSS

--- sphincs/xmss.py
+++ sphincs/xmss.py
@@ -1,5 +1,10 @@
 # implements eXtended Merkle Signature Scheme (XMSS)
 
+from wots import WOTS
+
+# initialize WOTS+ instance for XMSS
+wots = WOTS(32, 16)
+
 class XMSS:
     def __init__(self, h, n, w):
         self.h = h  # height (number of levels - 1) of the tree
@@ -18,6 +23,11 @@ class XMSS:
         """
         if (s % (1 << z)) != 0:
             raise ValueError("start index s must be a multiple of 2^z")
+
+        for i in range(2^z):
+            adrs.set_type(0)  # 0 is for WOTS+ hash address
+            adrs.set_keypair_addr(s + i)
+            node = wots.wots_PKgen(skseed, pkseed, adrs)
 
         # TODO: implement this function
         raise NotImplementedError("Not yet implemented")
