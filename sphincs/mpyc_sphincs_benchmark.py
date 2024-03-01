Fix Implementation bug in sign

sphincs/mpyc_sphincs_benchmark.py
@@ -164,7 +164,6 @@ async def main():
                         secfld.array(np.array([int(i) for i in sk_ele_bit[2]])),
                         secfld.array(np.array([int(i) for i in sk_ele_bit[3]]))]
 
-
             mes = input('Give the other input here: ')
             sk = in_
 
@@ -212,7 +211,7 @@ async def main():
         sig = await sphincs.sign(inputs[0][0], inputs[1], mes, sk)
         end = time.time()
     except AssertionError:
-        raise AssertionError("The length of the message and secret key is wrong! Please restart the function!")
+        print("The length of the message and secret key is wrong! Please restart the function!")
         await mpc.shutdown()
     except SyntaxError:
         print("Secret key value generated is wrong. Please restart the function and try again!")

sphincs/shake.py
@@ -131,7 +131,7 @@ class SHAKE:
         :param c: security parameter / capacity(?) (default 256)
         :return: SHAKE[c//2] of M with output length d
         """
-        if isinstance(M, mpc.SecureObject):
+        if not isinstance(M, mpc.SecureObject):
             M = util.to_secarray(M)
         # append 1111 to M
         N = np.concatenate((M, np.array([1, 1, 1, 1])))

sphincs/utils.py
@@ -14,10 +14,11 @@ class UTILS(object):
         :param x: input (typically string)
         :return: binary representation of type secure array in SecFld(2)
         """
-        # x should be of type string
-        bits = ''.join(format(ord(i), '08b') for i in str(x))
-        bitlist = [int(i) for i in bits]
-        return self.secfld.array(np.array(bitlist))
+        if isinstance(x, str):
+            # convert x to bytes
+            x = x.encode('utf-8')
+        x_bits = bin(int.from_bytes(x, byteorder='big')).replace("0b", "")
+        return self.secfld.array(np.array([int(i) for i in x_bits]))
 
     async def bits_to_bytestring(self, y):
         """