f264e1f6e8b2fb90ef952f5969afb5a282175341
devifaustine <devifaustine.df@gmail.com>
2024-03-02 01:40:01 +0100

    Fix bench script

--- bench_mpc_sphincs.sh
+++ bench_mpc_sphincs.sh
@@ -2,32 +2,43 @@
 
 #TODO: these will be the parameters (256 bytes or something and the messages or sk that will change for each loop
 
-# split array input by newlines
-IFS=$'\n'
+run_benchmark() {
+  # split array input by newlines
+  IFS=$'\n'
 
-#read -a arr <<< "$line"
-# number of iteration for n key pairs
-read -a keys <<< "$(python3 sphincs/gen_keys_bench.py -n 3)"
-#keys=$(python3 sphincs/gen_keys_bench.py -n 3)
-# number of iteration for n2 messages (for a key pair i)
-read -a mes <<< "$(python3 sphincs/gen_m_bench.py -n 5)"
-#mes=$(python3 sphincs/gen_m_bench.py -n 5)
+  # number of iteration for n key pairs
+  read -a keys <<< "$(python3 sphincs/gen_keys_bench.py -n 3)"
+  # number of iteration for n2 messages (for a key pair i)
+  read -a mes <<< "$(python3 sphincs/gen_m_bench.py -n 5)"
 
-for key in "${keys[@]}"; do
-  for m in "${mes[@]}"; do
-    python3 sphincs/mpyc_sphincs_benchmark.py -M2 -I0 <<< "$m
-    $key"&
-    # note: key has to be just the secret key, therefore public key has to be stored somewhere for verification
-    python3 sphincs/mpyc_sphincs_benchmark.py -M2 -I1 <<< "$key
-    $m"&
-    if [ $? -eq 0 ]; then
-      echo "Success"
-    else
-      echo "Failure"
-      set -e
-    fi
-  done
-done > log.txt
+  for key in "${keys[@]}"; do
+    for m in "${mes[@]}"; do
+      python3 sphincs/mpyc_sphincs_benchmark.py -M2 -I0 <<< "$m
+      $key"&
+      # note: key has to be just the secret key, therefore public key has to be stored somewhere for verification
+      python3 sphincs/mpyc_sphincs_benchmark.py -M2 -I1 <<< "$key
+      $m"&
+    done
+  done > log.txt
+}
+
+run_benchmark
+set -e
+
+exit_code=$?
+
+while true; do
+  run_benchmark
+  exit_code=$?
+
+  if [ $exit_code -eq 0 ]; then
+    echo "Running benchmark again."
+  else
+    echo "Benchmark successful."
+    break
+  fi
+done
+
 # TODO: make a table output in the command line for the results
 #  or put the result table benchmark in the log.txt file
 # exit the script
