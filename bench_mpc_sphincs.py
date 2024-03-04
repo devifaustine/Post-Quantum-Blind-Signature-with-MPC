8f57e1e99c49f78b1abf1e3f83fbf05837d5e4c5
Devi Faustine <64829752+devifaustine@users.noreply.github.com>
2024-03-04 00:06:34 +0000

    Change bench.py

--- bench_mpc_sphincs.py
+++ bench_mpc_sphincs.py
@@ -3,6 +3,9 @@
 
 import subprocess
 import time
+import threading
+import concurrent.futures
+
 
 logging = True
 
@@ -18,6 +21,50 @@ with open("keys.txt", "r") as file:
 user = ["python3", "sphincs/mpyc_sphincs_benchmark.py", "-M2", "-I0"]
 signer = ["python3", "sphincs/mpyc_sphincs_benchmark.py", "-M2", "-I1"]
 
+
+def run_user(instance_name, input_a, input_b, event):
+    process = subprocess.Popen(user, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # Wait until the other instance connects
+    event.wait()
+    # Provide inputs
+    process.stdin.write(f'{input_a}\n')
+    process.stdin.flush()
+    event.wait() 
+    process.stdin.write(f'{input_b}\n')
+    process.stdin.flush()
+    
+    # Get the output
+    output, _ = process.communicate()
+    print(f"Output of {instance_name} instance:", output)
+
+def run_signer(instance_name, input_a, input_b, event):
+    process = subprocess.Popen(signer, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # Wait until the other instance connects
+    event.wait()
+    # Provide inputs
+    process.stdin.write(f'{input_a}\n{input_b}\n')
+    process.stdin.flush()
+    
+    # Get the output
+    output, _ = process.communicate()
+    print(f"Output of {instance_name} instance:", output)
+
+# Create an event to coordinate the execution of both instances
+event = threading.Event()
+
+# Run both instances in parallel
+with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    future1 = executor.submit(run_user, "first", messages[0], keys[0], event)
+    future2 = executor.submit(run_signer, "second", keys[0], messages[0], event)
+
+    # Set the event to signal that both instances have connected
+    event.set()
+
+# Ensure that both instances have finished executing
+future1.result()
+future2.result()
+
+"""
 for i in range(len(keys)):
     for j in range(len(messages)):
         print("Signing message: ", messages[j], " with key: ", keys[i])
@@ -44,13 +91,7 @@ for i in range(len(keys)):
         while p1.poll() is None and p2.poll() is None:
             print("Process is still running. ")
             time.sleep(1)
-
-
-# wait for the processes to complete
-#p1.wait()
-#p2.wait()
-
-
+"""
 
 # process has completed
 print("Benchmark is done! Check bench_res.txt for results.")
