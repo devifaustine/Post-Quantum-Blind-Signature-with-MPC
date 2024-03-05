392aeff574990c2956795d1fa6d0a16652396e83
Devi Faustine <64829752+devifaustine@users.noreply.github.com>
2024-03-05 19:13:47 +0000

    Remove for loop in bench

--- bench_mpc_sphincs.py
+++ bench_mpc_sphincs.py
@@ -55,6 +55,7 @@ def run_signer(instance_name, input_a, input_b, event):
 # Create an event to coordinate the execution of both instances
 event = threading.Event()
 
+
 # Run both instances in parallel
 with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
     future1 = executor.submit(run_user, "first", messages[0], keys[0], event)
@@ -70,30 +71,18 @@ future2.result()
 """
 for i in range(len(keys)):
     for j in range(len(messages)):
-        print("Signing message: ", messages[j], " with key: ", keys[i])
-        
-       # Starts both processes
-        p1 = subprocess.run(user, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        p2 = subprocess.run(signer, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Run both instances in parallel
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            future1 = executor.submit(run_user, "first", messages[0], keys[0], event)
+            future2 = executor.submit(run_signer, "second", keys[0], messages[0], event)
 
-        # provide input for user
-        p1.stdin.write(messages[0]+'\n')
-        p1.stdin.flush()
-        p1.stdin.write(keys[0]+'\n')
-        p1.stdin.flush()
+            # Set the event to signal that both instances have connected
+            event.set()
 
-        # provide input for signer
-        p2.stdin.write(keys[0]+'\n')
-        p2.stdin.flush()
-        p2.stdin.write(messages[0]+'\n')
-        p2.stdin.flush()
+        # Ensure that both instances have finished executing
+        future1.result()
+        future2.result()
 
-        output_user, error_user = p1.communicate()
-        output_signer, error_signer = p2.communicate()
-
-        while p1.poll() is None and p2.poll() is None:
-            print("Process is still running. ")
-            time.sleep(1)
 """
 
 # process has completed
